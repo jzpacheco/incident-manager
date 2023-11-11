95826ad2f2dc9d83f4c1522d2c2a6aa0e6b51d89
jzpacheco <jeffersonpacheco@hotmail.com>
2023-11-11 10:01:45 -0300

    Initializing spring project

---
+++ src/test/java/com/jzpacheco/incidentmanager/IncidentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzpacheco.incidentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
